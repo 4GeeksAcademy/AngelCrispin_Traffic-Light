692b488802ef18fe71aaab3ff98d1d16fd5b06ca
Angel Crispin <155650252+AngelCrispin@users.noreply.github.com>
2024-08-13 18:16:43 +0000

    First Commit

---
+++ src/js/component/trafficLight.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react'
+
+
+ const TrafficLight=(props)=>{
+
+
+    const [lights, setLights] = useState(["","",""]);
+
+    const TurnOnLight = (index) => {
+        const lightsAux = ["","",""];
+        lightsAux[index]="light";
+        setLights(lightsAux);
+        //console.log(typeof(lights));
+        //console.log(typeof(lightsAux));
+    }
+
+    return(
+        <div className="trafficLight">
+            <div className="rectangle mx-auto"> 
+            </div>
+            <div className="square mx-auto"> 
+                <div className={`circle bg-danger m-auto ${lights[0]}`} onClick={()=>{TurnOnLight(0)}}></div>
+                <div className={`circle bg-warning m-auto ${lights[1]}`} onClick={()=>{TurnOnLight(1)}}></div>
+                <div className={`circle bg-success m-auto ${lights[2]}`} onClick={()=>{TurnOnLight(2)}}></div>
+            </div>
+        </div>
+    );
+}
+
+
+export default TrafficLight;
